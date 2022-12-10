885615022bbca71f566e27445985b8a8977bd606
Hasan Huseynli <hasan@Hasans-MacBook-Pro.local>
2022-12-10 09:52:28 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+{
+    Console.WriteLine("a ededini daxil et");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("b ededini daxil et");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a + b);
+
+}
